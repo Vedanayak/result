a7ac93fb7bac91dda1b75c62cfa682d9914fb9f4
Vedanayak <121534914+Vedanayak@users.noreply.github.com>
2023-08-19 12:13:15 +0530

    Update hello23.py

--- hello23.py
+++ hello23.py
@@ -1,4 +1,4 @@
 import streamlit  as st
-st.write("hello world")
+st.write("helloworld")
 st.header("power by nikhil")
 st.write("[learn more>](https://www.youtube.com/watch?v=VqgUkExPvLY&list=LL&index=1&t=10s)")
